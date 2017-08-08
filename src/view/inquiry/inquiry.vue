3bbcfc57eff7b145095f15add5fd640864bdc7b6
unknown <1134974025@qq.com>
2017-08-08 14:49:37 +0800

    88

--- src/view/inquiry/inquiry.vue
+++ src/view/inquiry/inquiry.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="contain">
+    <div class="in-location">
+     <!--  <h2>当前位置：<span>问诊</span></h2> -->
+    </div>
+
     <div class="in-nav">
       <el-row class="containRow" :gutter="20">
         <el-col :span="6">
@@ -9,11 +13,11 @@
           </div>
         </el-col>
 
-        <el-col :span="8" :offset="10">
+        <el-col :span="3" :offset="12">
           <div class="grid-content bg-purple">
-            <span><a href=""><i class="fa fa-print" aria-hidden="true"></i>打印预览</a></span>
+            <!-- <span><a href=""><i class="fa fa-print" aria-hidden="true"></i>打印预览</a></span> -->
             <div class="in-btn">
-              <el-button>保存</el-button>
+              <!-- <el-button>保存</el-button> -->
               <el-button type="primary">结束就诊</el-button>
             </div>
           </div>
@@ -29,68 +33,84 @@
               <img src="../../assets/in-info.png">
               <div class="in-file">
                 <span>李春华</span>
-                <span><a href=""><i class="fa fa-file" aria-hidden="true"></i>查看档案</a></span>
+                <!-- <span><a href=""><i class="fa fa-file" aria-hidden="true"></i>查看档案</a></span> -->
               </div>
               <div class="in-table">
-                <div class="in-tabOne"><h2>姓名:<span>女</span></h2></div>
-                <div class="in-tabTwo"><h2>年龄:<span>46</span></h2></div>
+                <div class="in-tabOne"><h2>性别:<span>{{ this.info.sex }}</span></h2></div>
+                <div class="in-tabTwo"><h2>年龄:<span>{{ this.info.age }}</span></h2></div>
               </div>
               <div class="in-tel">
-                <h2>电话:<span>13012345678</span></h2>
+                <h2>电话:<span>{{ this.info.tel }}</span></h2>
               </div>
               <div class="in-remark">
               <h2>备注:</h2>
-                <p>文字描述很长很长文字描述很长很长文字描述很长很长文字描述很长很长文字描述很长很长文字描述很长很长文字描述很长很长文字描述很长很长</p>
+                <p>{{ this.info.remark }}</p>
               </div>
             </div>
 
             <div class="in-con in-conTwo">
               <div class="in-header">
                 <h2>本次测量值</h2>
-                <span><a href=""><i class="fa fa-pencil-square-o" aria-hidden="true"></i>编辑</a></span>
+                <span @click="handleEdit()"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>编辑</span>
               </div>
               <div class="in-text">
-                <h2>身&nbsp;&nbsp;&nbsp;高:<span>--</span></h2>
-                <span><a href=""><i class="fa fa-line-chart" aria-hidden="true"></i></a></span>
+                <h2>身&nbsp;&nbsp;&nbsp;高:<span v-model="this.info.height">{{ this.infodata.height }}</span></h2>
               </div>
               <div class="in-text">
-                <h2>体&nbsp;&nbsp;&nbsp;重:<span>--</span></h2>
-                <span><a href=""><i class="fa fa-line-chart" aria-hidden="true"></i></a></span>
+                <h2>体&nbsp;&nbsp;&nbsp;重:<span>{{ this.infodata.weight }}</span></h2>
+                <!-- <span><i class="fa fa-line-chart" aria-hidden="true"></i></span> -->
               </div>
               <div class="in-text">
-                <h2>体&nbsp;&nbsp;&nbsp;温:<span>--</span></h2>
+                <h2>体&nbsp;&nbsp;&nbsp;温:<span>{{ this.infodata.teper }}</span></h2>
+                <!-- <span><i class="fa fa-line-chart" aria-hidden="true"></i></span> -->
               </div>
               <div class="in-text">
-                <h2>舒张压:<span>--</span></h2>
+                <h2>舒张压:<span>{{ this.infodata.pressure }}</span></h2>
               </div>
               <div class="in-text">
-                <h2>收缩压:<span>--</span></h2>
+                <h2>收缩压:<span>{{ this.infodata.systolic }}</span></h2>
               </div>
             </div>
+
+            <!--编辑界面-->
+            <div>
+              <el-dialog title="测量值编辑" :visible.sync="dialogEditVisible" :close-on-click-modal="false">
+                <el-form label-position="left" label-width="60px" style='width: 600px; margin-left:50px;' :model="editinfo">
+                  <el-form-item label="身高" prop="height">
+                    <el-input v-model="editinfo.height"></el-input>
+                  </el-form-item>
+                  <el-form-item label="体重" prop="weight">
+                    <el-input v-model="editinfo.weight"></el-input>
+                  </el-form-item>
+                  <el-form-item label="体温" prop="teper">
+                    <el-input v-model="editinfo.teper"></el-input>
+                  </el-form-item>
+                  <el-form-item label="舒张压" prop="pressure">
+                    <el-input v-model="editinfo.pressure"></el-input>
+                  </el-form-item>
+                  <el-form-item label="收缩压" prop="systolic">
+                    <el-input v-model="editinfo.systolic"></el-input>
+                  </el-form-item>
+                  </el-form>
+                  <div slot="footer" class="dialog-footer">
+                    <el-button type="primary" @click="editSubmit()">确定</el-button>
+                    <el-button type="primary" @click="editReset()">重置</el-button>
+                    <el-button @click="editCancel()">取消</el-button>
+                  </div>
+              </el-dialog>
+            </div>    
 
             <div class="in-con in-conThree">
               <div class="in-past">
                 <h2>过往病史</h2>
-                <div class="in-date">
-                  <el-date-picker v-model="value6" type="daterange" placeholder="选择日期范围">
-                  </el-date-picker>
-                </div>
               </div>
-              <div class="in-disease">
-                <p class="in-disTitle">急性坏疽性阑尾炎伴穿孔</p>
-                <h2 class="in-disDate">2017-04-25&nbsp;&nbsp;11:55:50</h2>
-                <div class="in-disBtn">
+              <div class="in-disease" v-for="item in diseaseData">
+                <p class="in-disTitle">{{ item.disname }}</p>
+                <h2 class="in-disDate">{{ item.disdate}}&nbsp;&nbsp;{{ item.distime }}</h2>
+                <!-- <div class="in-disBtn">
                   <span><a href=""><i class="fa fa-file-text-o" aria-hidden="true"></i>详情</a></span>
                   <span><a href=""><i class="fa fa-clone" aria-hidden="true"></i>复制病历</a></span>
-                </div>
-              </div>
-              <div class="in-disease">
-                <p class="in-disTitle">副伤寒</p>
-                <h2 class="in-disDate">2017-04-12&nbsp;&nbsp;10:00:00</h2>
-                <div class="in-disBtn">
-                  <span><a href=""><i class="fa fa-file-text-o" aria-hidden="true"></i>详情</a></span>
-                  <span><a href=""><i class="fa fa-clone" aria-hidden="true"></i>复制病历</a></span>
-                </div>
+                </div> -->
               </div>
               <div class="in-disease in-disNone">
                 <h2>没有更多的诊断结果</h2>
@@ -114,12 +134,72 @@
   export default {
     data() {
       return {
-        value6: ''
-
+        value6: '',
+        info:{
+          name:'李春华',
+          sex:'女',
+          age:40,
+          tel:'13012345678',
+          remark:'无'
+        },
+        infodata:{
+          height:'--',
+          weight:'--',
+          teper:'--',
+          pressure:'--',
+          systolic:'--'
+        },
+        dialogEditVisible:false,
+        editinfo:{
+          height:'',
+          weight:'',
+          teper:'',
+          pressure:'',
+          systolic:''
+        },
+        diseaseData:[
+          {disname:'急性坏疽性阑尾炎伴穿孔',disdate:'2017-04-25',distime:'11:55:50'},
+          {disname:'副伤寒',disdate:'2017-04-12',distime:'10:00:00'}
+        ]
       }
     },
     components:{
       inquiryone:InquiryOne
+    },
+    methods:{
+      //编辑界面显示
+      handleEdit() {
+        this.dialogEditVisible = true;
+        this.infodata = JSON.parse(JSON.stringify(this.editinfo))
+      },
+      //编辑界面里的确定提交按钮
+      editSubmit(){
+        let that = this
+        that.$confirm('确认提交吗？', '提示', {}).then(() => {
+          that.$message({
+            type: 'success',
+            message: '修改成功'
+          });
+          that.infodata = that.editinfo;
+          that.dialogEditVisible = false;
+        }).catch(() => {
+      
+        });
+      },
+      //编辑界面的重置按钮
+      editReset() {
+        this.editinfo = {
+          height:'',
+          weight:'',
+          teper:'',
+          pressure:'',
+          systolic:''
+        }
+      },
+      //编辑界面里的取消按钮
+      editCancel(){
+        this.dialogEditVisible = false;
+      }
     }
   }
 </script>
@@ -133,6 +213,12 @@
   .contain a{
     color: blue;
     text-decoration: none;
+  }
+  .in-location h2{
+    font-weight: none;
+    font-size: 16px;
+    color:#dcdcdc;
+    padding:10px 0 0 10px;
   }
   .containRow{
     margin: 0 !important;
@@ -241,6 +327,7 @@
     display: inline-block;
     float: right;
     margin-right: 28px;
+    cursor: pointer;
   }
   .in-header span i{
     margin-right: 10px;
@@ -262,12 +349,14 @@
   .in-text span{
     float: right;
     margin-right: 28px;
+    color:blue;
+    cursor: pointer;
   }
   .in-conThree{
     text-align: left;
   }
   .in-past{
-    
+    margin-bottom: 12px;
   }
   .in-date{
     margin: 8px 0 10px 21px;
